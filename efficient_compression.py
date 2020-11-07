c54869e49d95b4481059a62476d0e36cdba16ea1
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2020-11-07 17:36:06 +0900

    Update efficient_compression.py

--- efficient_compression.py
+++ efficient_compression.py
@@ -51,6 +51,25 @@ def rd_conv_pruning(rd_conv, prune_list):
     pruned_rd_conv.weight.data = pruned_rd_conv.weight.data.cuda()
     return pruned_rd_conv
 
+def bn_pruning(bn, prune_list):
+    num_features = bn.num_features
+    pruned_bn = torch.nn.BatchNorm2d(num_features=bn.num_features - len(prune_list), eps=bn.eps, momentum=bn.momentum, affine=bn.affine, track_running_stats=bn.track_running_stats)
+    
+    bn_weight = bn.weight.data.cpu().numpy() 
+    pruned_bn_weight = pruned_bn.weight.data.cpu().numpy() 
+    
+    bn_bias = bn.bias.data.cpu().numpy() 
+    pruned_bn_bias = pruned_bn_bias = pruned_bn.bias.data.cpu().numpy() 
+    
+    puned_bn_weight = numpy.delete(bn_weight, prune_list) 
+    pruned_bn_bias = numpy.delete(bn_bias, prune_list) 
+    
+    pruned_bn.weight.data = torch.from_numpy(pruned_bn_weight)
+    pruned_bn.weight.data = pruned_bn.weight.data.cuda() 
+    
+    pruned_bn.bias.data = torch.from_numpy(pruned_bn_bias)
+    pruned_bn.bias.data = pruned_bn.bias.data.cuda()
+    return pruned_bn 
 
 def get_prune_list(conv,next_conv,cut_off_rate):
     importance_list = numpy.array([])
@@ -70,36 +89,52 @@ def get_prune_list(conv,next_conv,cut_off_rate):
         prune_list = sorted(range(len(importance_list)),key=lambda i: importance_list[i])[:int(conv.out_channels*cut_off_rate)] 
     return prune_list
 
-blocks = range(2,23)
+def pruning_efficient_net(model,save_name):
+    original_parameters = sum(p.numel() for p in model.parameters() if p.required_grad) 
+    
+    #blocks = range(2,23) 
+    blocks = range(2,16) 
+    for i in blocks:
+        ex_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_expand_conv']._modules['conv']
+        bn0 = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_bn0']
+        dw_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_depthwise_conv']._modules['conv'] 
+        bn1 = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_bn1']
+        se_rd_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_reduce']._modules['conv']
+        se_ex_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_expand']._modules['conv']
+        rd_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_project_conv']._modules['conv']
 
-for i in blocks:
-    ex_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_expand_conv']._modules['conv']
-    dw_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_depthwise_conv']._modules['conv'] 
-    se_rd_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_reduce']._modules['conv']
-    se_ex_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_expand']._modules['conv']
-    rd_conv = model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_project_conv']._modules['conv']
+        prune_list = get_prune_list(dw_conv, 0.5)
 
-    prune_list = get_prune_list(dw_conv, 0.5)
+        print("------------------------------------")
+        print(ex_conv.weight.data.size())
+        print(dw_conv.weight.data.size())
+        print(se_rd_conv.weight.data.size())
+        print(se_ex_conv.weight.data.size())
+        print(rd_conv.weight.data.size())
+        print("-------------------------------------") 
+        pruned_ex_conv = ex_conv_pruning(ex_conv, prune_list)
+        pruned_bn0 = bn_pruning(bn0, prune_list) 
+        pruned_dw_conv = dw_conv_pruning(dw_conv, prune_list)
+        pruned_bn1 = bn_pruning(bn1, prune_list) 
+        pruned_se_rd_conv = rd_conv_pruning(se_rd_conv, prune_list)
+        pruned_se_ex_conv = ex_conv_pruning(se_ex_conv, prune_list)
+        pruned_rd_conv = rd_conv_pruning(rd_conv, prune_list) 
+        print(pruned_ex_conv.weight.data.size()) 
+        print(pruned_dw_conv.weight.data.size())
+        print(pruned_se_rd_conv.weight.data.size())
+        print(pruned_se_ex_conv.weight.data.size())
+        print(pruned_rd_conv.weight.data.size()) 
+        print("-------------------------------------")
 
-    print("------------------------------------")
-    print(ex_conv.weight.data.size())
-    print(dw_conv.weight.data.size())
-    print(se_rd_conv.weight.data.size())
-    #print(se_ex_conv.weight.data.size())
-    #print(rd_conv.weight.data.size())
-    print("-------------------------------------") 
-    pruned_ex_conv = ex_conv_pruning(ex_conv, prune_list)
-    pruned_dw_conv = dw_conv_pruning(dw_conv, prune_list)
-    pruned_rd_conv = rd_conv_pruning(se_rd_conv, prune_list) 
-    print(pruned_ex_conv.weight.data.size()) 
-    print(pruned_dw_conv.weight.data.size())
-    print(pruned_se_rd_conv.weight.data.size())
-    print("-------------------------------------")
-
-    model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_expand_conv']._modules['conv'] = pruned_ex_conv
-    model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_depthwise_conv']._modules['conv'] = pruned_dw_conv
-    model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_reduce']._modules['conv'] = pruned_se_rd_conv
-
-torch.save(model, args.save_name)
-print(sum(p.numel()  for p in model.parameters() if p.requires_grad))
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_expand_conv']._modules['conv'] = pruned_ex_conv
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_bn0'] = pruned_bn0
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_depthwise_conv']._modules['conv'] = pruned_dw_conv
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_bn1'] = pruned_bn1
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_reduce']._modules['conv'] = pruned_se_rd_conv
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_se_expand']._modules['conv'] = pruned_se_ex_conv 
+        model._modules['backbone_net']._modules['model']._modules['_blocks']._modules[str(i)]._modules['_project_conv']._modules['conv'] = pruned_rd_conv
+    
+    pruned_parameters = sum(p.numle() for p in model.parameters() if p.requires_grad) 
+    torch.save(model,save_name) 
+    return pruned_parameters
 
