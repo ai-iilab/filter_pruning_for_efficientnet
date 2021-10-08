6d4d4c48cf9fd226435c1642b2bf76d45ba08790
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2021-10-08 18:49:28 +0900

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -15,3 +15,27 @@ def test_quantize_linear():
     vals = set(param.view(param.numel()).tolist())
     for v in vals:
         assert v in centers_
+
+        
+def test_quantize_linear_fix_zeros():
+    param = torch.rand(128, 64, 3, 3) - 0.5
+    mask = prune_vanilla_elementwise(sparsity=0.4, param=param)
+    codebook = quantize_linear_fix_zeros(param, k=16)
+    assert codebook['cluster_centers_'].numel() == 16
+    centers_ = codebook['cluster_centers_'].tolist()
+    vals = set(param.view(param.numel()).tolist())
+    for v in vals:
+        assert v in centers_
+    assert param.masked_select(mask).eq(0).all()
+    
+    
+def test_quantized_fixed_point():
+    param = torch.rand(128, 64, 3, 3) - 0.5
+    mask = prune_vanilla_elementwise(sparsity=0.4, param=param)
+    codebook = quantize_fixed_point(param, bit_length=8, bit_length_integer=1)
+    assert codebook['cluster_centers_'].numel() == 2 ** 8
+    centers_ = codebook['cluster_centers_'].tolist()
+    vals = set(param.view(param.numel()).tolist())
+    for v in vals:
+        assert v in centers_
+    assert param.masked_select(mask).eq(0).all()
