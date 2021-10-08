2602c81d7c275488abad1cc9c7647b8da23b28c0
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2021-10-08 18:52:02 +0900

    Update basic_pytorch.py

--- basic_pytorch.py
+++ basic_pytorch.py
@@ -21,6 +21,21 @@ def test_prune_vanilla_elementwise():
     assert mask.sum() == int(math.ceil(param.numel() * 0.7))
     assert param.masked_select(mask).eq(0).all()
 
+    
+    def test_prune_vanilla_kernelwise():
+    param = torch.rand(64, 128, 3, 3)
+    mask = prune_vanilla_kernelwise(sparsity=0.5, param=param)
+    mask_s = mask.view(64*128, -1).all(1).sum()
+    assert mask_s == 32*128
+    assert param.masked_select(mask).eq(0).all()
+
+
+def test_prune_vanilla_filterwise():
+    param = torch.rand(64, 128, 3, 3)
+    mask = prune_vanilla_filterwise(sparsity=0.5, param=param)
+    mask_s = mask.view(64, -1).all(1).sum()
+    assert mask_s == 32
+    assert param.masked_select(mask).eq(0).all()
 
 
 x = torch.rankd(2,3,4)
