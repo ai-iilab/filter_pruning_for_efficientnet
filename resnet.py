18f52a3895d543b6e596bb1010285adeaa5bf060
26binary <80504323+26binary@users.noreply.github.com>
2021-10-08 19:26:15 +0900

    Update resnet.py
    class basicblock

--- resnet.py
+++ resnet.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+class basicblock(nn.Module):
+    """Basic Block for Resnet 18 and Resnet 34
+
+    """
+
+    #basicblock and bottleneck block
+    #have different output size
+    #we use class attribute expansion
+    #to distinct
+    
