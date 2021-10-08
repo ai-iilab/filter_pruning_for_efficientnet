2bbd94f51713c6abc7f466e307e577bb9e94b88f
hongkkab <43055704+hongkkab@users.noreply.github.com>
2021-10-08 14:09:55 +0900

    Update hard_sample.py
    add some functions for hard prune

--- hard_sample.py
+++ hard_sample.py
@@ -153,4 +153,58 @@ def index_remove(tensor, dim, index, removed=False):
 
     return new_tensor
 
+def get_new_conv(conv, dim, channel_index, independent_prune_flag=False):
+    if dim == 0:
+        new_conv = torch.nn.Conv2d(in_channels=conv.in_channels,
+                                   out_channels=int(conv.out_channels - len(channel_index)),
+                                   kernel_size=conv.kernel_size,
+                                   stride=conv.stride, padding=conv.padding, dilation=conv.dilation, bias=conv.bias is not None)
+
+        new_conv.weight.data = index_remove(conv.weight.data, dim, channel_index)
+        if conv.bias is not None:
+            new_conv.bias.data = index_remove(conv.bias.data, dim, channel_index)
+
+        return new_conv
+
+    elif dim == 1:
+        new_conv = torch.nn.Conv2d(in_channels=int(conv.in_channels - len(channel_index)),
+                                   out_channels=conv.out_channels,
+                                   kernel_size=conv.kernel_size,
+                                   stride=conv.stride, padding=conv.padding, dilation=conv.dilation, bias=conv.bias is not None)
+
+        new_weight = index_remove(conv.weight.data, dim, channel_index, independent_prune_flag)
+        residue = None
+        if independent_prune_flag:
+            new_weight, residue = new_weight
+        new_conv.weight.data = new_weight
+        if conv.bias is not None:
+            new_conv.bias.data = conv.bias.data
+
+        return new_conv, residue
+
+
+def get_new_norm(norm, channel_index):
+    new_norm = torch.nn.BatchNorm2d(num_features=int(norm.num_features - len(channel_index)),
+                                    eps=norm.eps,
+                                    momentum=norm.momentum,
+                                    affine=norm.affine,
+                                    track_running_stats=norm.track_running_stats)
+    new_norm.weight.data = index_remove(norm.weight.data, 0, channel_index)
+    new_norm.bias.data = index_remove(norm.bias.data, 0, channel_index)
+
+    if norm.track_running_stats:
+        new_norm.running_mean.data = index_remove(norm.running_mean.data, 0, channel_index)
+        new_norm.running_var.data = index_remove(norm.running_var.data, 0, channel_index)
+
+    return new_norm
+
+
+def get_new_linear(linear, channel_index):
+    new_linear = torch.nn.Linear(in_features=int(linear.in_features - len(channel_index)),
+                                 out_features=linear.out_features,
+                                 bias=linear.bias is not None)
+    new_linear.weight.data = index_remove(linear.weight.data, 1, channel_index)
+    new_linear.bias.data = linear.bias.data
+
+    return new_linear
 
