ee23498be94eaa093299ee3b0de166983963238f
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2021-10-08 18:54:52 +0900

    Update pruning_for_DNN.py

--- pruning_for_DNN.py
+++ pruning_for_DNN.py
@@ -13,3 +13,17 @@ def test_prune_vanilla_elementwise():
     mask = prune_vanilla_elementwise(sparsity=0.7, param=param)
     assert mask.sum() == int(math.ceil(param.numel() * 0.7))
     assert param.masked_select(mask).eq(0).all()
+
+def test_prune_vanilla_kernelwise():
+    param = torch.rand(64, 128, 3, 3)
+    mask = prune_vanilla_kernelwise(sparsity=0.5, param=param)
+    mask_s = mask.view(64*128, -1).all(1).sum()
+    assert mask_s == 32*128
+    assert param.masked_select(mask).eq(0).all()
+    
+def test_prune_vanilla_filterwise():
+    param = torch.rand(64, 128, 3, 3)
+    mask = prune_vanilla_filterwise(sparsity=0.5, param=param)
+    mask_s = mask.view(64, -1).all(1).sum()
+    assert mask_s == 32
+    assert param.masked_select(mask).eq(0).all()
