b87d761dc3029788971c46d0bcb6a16aa0d1d21f
hongkkab <43055704+hongkkab@users.noreply.github.com>
2021-10-08 14:02:13 +0900

    Update and rename geometric_mean to geometric_mean.py
    rename and adding a function

---
+++ geometric_mean.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models
+from utils import convert_secs2time, time_string, time_file_str, timing
+# from models import print_log
+import models
+import random
+import numpy as np
+from scipy.spatial import distance
+from collections import OrderedDict
+
+class Mask:
+    def __init__(self, model):
+        self.model_size = {}
+        self.model_length = {}
+        self.compress_rate = {}
+        self.distance_rate = {}
+        self.mat = {}
+        self.model = model
+        self.mask_index = []
+        self.filter_small_index = {}
+        self.filter_large_index = {}
+        self.similar_matrix = {}
+
+    def get_codebook(self, weight_torch, compress_rate, length):
+        weight_vec = weight_torch.view(length)
+        weight_np = weight_vec.cpu().numpy()
+
+        weight_abs = np.abs(weight_np)
+        weight_sort = np.sort(weight_abs)
+
+        threshold = weight_sort[int(length * (1 - compress_rate))]
+        weight_np[weight_np <= -threshold] = 1
+        weight_np[weight_np >= threshold] = 1
+        weight_np[weight_np != 1] = 0
+
+        print("codebook done")
+        return weight_np
+
+  
+  def get_filter_codebook(self, weight_torch, compress_rate, length):
+        codebook = np.ones(length)
+        if len(weight_torch.size()) == 4:
+            filter_pruned_num = int(weight_torch.size()[0] * (1 - compress_rate))
+            weight_vec = weight_torch.view(weight_torch.size()[0], -1)
+            # norm1 = torch.norm(weight_vec, 1, 1)
+            # norm1_np = norm1.cpu().numpy()
+            norm2 = torch.norm(weight_vec, 2, 1)
+            norm2_np = norm2.cpu().numpy()
+            filter_index = norm2_np.argsort()[:filter_pruned_num]
+            #            norm1_sort = np.sort(norm1_np)
+            #            threshold = norm1_sort[int (weight_torch.size()[0] * (1-compress_rate) )]
+            kernel_length = weight_torch.size()[1] * weight_torch.size()[2] * weight_torch.size()[3]
+            for x in range(0, len(filter_index)):
+                codebook[filter_index[x] * kernel_length: (filter_index[x] + 1) * kernel_length] = 0
+            print("filter codebook done")
+        elif len(weight_torch.size()) == 2:
+            weight_torch = weight_torch.view(weight_torch.size()[0], weight_torch.size()[1], 1, 1)
+            codebook = self.get_filter_codebook(weight_torch, compress_rate, length)
+            print("filter codebook for fc done")
+        else:
+            pass
+        return codebook
+
+    @timing
+    def get_filter_similar_old(self, weight_torch, compress_rate, distance_rate, length):
+        codebook = np.ones(length)
+        if len(weight_torch.size()) == 4:
+            filter_pruned_num = int(weight_torch.size()[0] * (1 - compress_rate))
+            similar_pruned_num = int(weight_torch.size()[0] * distance_rate)
+            weight_vec = weight_torch.view(weight_torch.size()[0], -1)
+            # norm1 = torch.norm(weight_vec, 1, 1)
+            # norm1_np = norm1.cpu().numpy()
+            norm2 = torch.norm(weight_vec, 2, 1)
+            norm2_np = norm2.cpu().numpy()
+            filter_small_index = []
+            filter_large_index = []
+            filter_large_index = norm2_np.argsort()[filter_pruned_num:]
+            filter_small_index = norm2_np.argsort()[:filter_pruned_num]
+            print('weight_vec.size', weight_vec.size())
+            # distance using pytorch function
+            similar_matrix = torch.zeros((len(filter_large_index), len(filter_large_index)))
+            for x1, x2 in enumerate(filter_large_index):
+                for y1, y2 in enumerate(filter_large_index):
+                    # cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+                    # similar_matrix[x1, y1] = cos(weight_vec[x2].view(1, -1), weight_vec[y2].view(1, -1))[0]
+                    pdist = torch.nn.PairwiseDistance(p=2)
+                    # print('weight_vec[x2].size', weight_vec[x2].size())
+                    similar_matrix[x1, y1] = pdist(weight_vec[x2].view(1, -1), weight_vec[y2].view(1, -1))[0][0]
+                    # print('weight_vec[x2].size after', weight_vec[x2].size())
+            # more similar with other filter indicates large in the sum of row
+            similar_sum = torch.sum(torch.abs(similar_matrix), 0).numpy()
+
+            # for distance similar: get the filter index with largest similarity == small distance
+            similar_large_index = similar_sum.argsort()[similar_pruned_num:]
+            similar_small_index = similar_sum.argsort()[:  similar_pruned_num]
+            similar_index_for_filter = [filter_large_index[i] for i in similar_small_index]
+
+            print('filter_large_index', filter_large_index)
+            print('filter_small_index', filter_small_index)
+            print('similar_sum', similar_sum)
+            print('similar_large_index', similar_large_index)
+            print('similar_small_index', similar_small_index)
+            print('similar_index_for_filter', similar_index_for_filter)
+            kernel_length = weight_torch.size()[1] * weight_torch.size()[2] * weight_torch.size()[3]
+            for x in range(0, len(similar_index_for_filter)):
+                codebook[
+                similar_index_for_filter[x] * kernel_length: (similar_index_for_filter[x] + 1) * kernel_length] = 0
+            print("similar index done")
+        else:
+            pass
+        return codebook
+
+    # optimize for fast ccalculation
+ 
