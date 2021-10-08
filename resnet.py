6bd22074c0d288548c773bc967ce7f9d36d7a2fb
26binary <80504323+26binary@users.noreply.github.com>
2021-10-08 19:29:33 +0900

    Update resnet.py
    bottleneck init residual_function update

--- resnet.py
+++ resnet.py
@@ -44,3 +44,13 @@ class bottleneck(nn.Module):
     expansion = 4
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels * bottleneck.expansion, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels * bottleneck.expansion),
+        )
