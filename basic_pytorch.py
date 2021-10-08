b10b05b74203ce8a5c7b4ad45876e59c46408c31
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2021-10-08 12:14:21 +0900

    Update basic_pytorch.py

--- basic_pytorch.py
+++ basic_pytorch.py
@@ -12,3 +12,28 @@ scalar_x = x[1,1,1]
 #numpy like slicing 
 x = torch.rand(2,3)
 print(x[:,1:]) #skipping first column
+
+"""
+A Simple Neural Network
+
+Learning the PyTorch way of building a neural network is really important. 
+IT is the most efficient and clean way of writting PyTorch code, and it also helps you to find
+tutorials and sample snippets easy to follow, since they have the same structure
+"""
+
+def binary_encoder(input_size):
+  def wrapper(num):
+    ret = [int(i) for i in '{0:b}'.format(num)]
+    return [0] * (input_size - len(ret)) + ret
+  
+def get_numpy_data(input_size=10, limit=1000):
+  x = []
+  y = []
+  encoder = binary_encoder(input_size)
+  for i in range(limit):
+    x.append(encoder(i))
+    if i % 15 == 0:
+      y.append([1,0,0,0])
+    elif i % 5 == 0:
+      y.append([0,1,0,0])
+  return training_test_gen(np.array(x), np.array(x))
