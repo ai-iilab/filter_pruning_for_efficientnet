a7b21fa7d1a298e4091dbfb3aa062f014280deee
TaehyeonKim-pyomu <60841726+TaehyeonKim-pyomu@users.noreply.github.com>
2021-10-08 18:55:13 +0900

    Update pruning_for_DNN.py

--- pruning_for_DNN.py
+++ pruning_for_DNN.py
@@ -27,3 +27,26 @@ def test_prune_vanilla_filterwise():
     mask_s = mask.view(64, -1).all(1).sum()
     assert mask_s == 32
     assert param.masked_select(mask).eq(0).all()
+    
+def test_vanilla_pruner():
+    rule = [
+        ('0.weight', 'element', [0.3, 0.5]),
+        ('1.weight', 'element', [0.4, 0.6])
+    ]
+    rule_dict = {
+        '0.weight': [0.3, 0.5],
+        '1.weight': [0.4, 0.6]
+    }
+    model = torch.nn.Sequential(torch.nn.Conv2d(256, 128, 3, bias=True),
+                                torch.nn.Conv2d(128, 512, 1, bias=False))
+    pruner = VanillaPruner(rule=rule)
+    pruner.prune(model=model, stage=0, verbose=True)
+    for n, param in model.named_parameters():
+        if param.dim() > 1:
+            mask = pruner.masks[n]
+            assert mask.sum() == int(math.ceil(param.numel() * rule_dict[n][0]))
+            assert param.data.masked_select(mask).eq(0).all()
+    state_dict = pruner.state_dict()
+    pruner = VanillaPruner().load_state_dict(state_dict)
+    model = torch.nn.Sequential(torch.nn.Conv2d(256, 128, 3, bias=True),
+                                torch.nn.Conv2d(128, 512, 1, bias=False))
